050aa753c82939c6af552bafb5e1d221ecf538ae
trvrle <32115579+trvrle@users.noreply.github.com>
2018-06-11 17:41:44 -0600

    Updated nav-bar

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use('/static', express.static('public'))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/index.html'))
+  res.sendFile(path.join(__dirname + '/pages/home/home.html'))
 })
 
 app.get('/header.html', function(req, res) {
   res.sendFile(path.join(__dirname + '/pages/navigation/header.html'))
 })
 
+app.get('/home.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/static/home.css'))
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
