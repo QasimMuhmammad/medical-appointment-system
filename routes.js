e20524104e843f4d533cd1e4b33e3aa763ae9223
trvrle <32115579+trvrle@users.noreply.github.com>
2018-06-13 17:18:36 -0600

    fixed file routing

--- routes.js
+++ routes.js
@@ -8,30 +8,34 @@ router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/pages/home/home.html'))
 })
 
-router.get('/header.html', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/header.html'))
+router.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/login.html'))
 })
 
-router.get('/home.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/static/home.css'))
+router.get('/dashboard', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/dashboard.html'))
+})
+
+router.get('/header.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/navigation/header.html'))
 })
 
 router.get('/footer.html', function(req, res) {
   res.sendFile(path.join(__dirname + '/pages/navigation/footer.html'))
 })
 
+router.get('/home.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/css/home.css'))
+})
+
 router.get('/signin.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/signin.css'))
+  res.sendFile(path.join(__dirname + '/public/css/signin.css'))
 })
 
 router.get('/footer.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/footer.css'))
+  res.sendFile(path.join(__dirname + '/public/css/footer.css'))
 })
 
-router.get('/home', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/index.html'))
-})
-
-router.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/login.html'))
+router.get('/dashboard.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/css/dashboard.css'))
 })
