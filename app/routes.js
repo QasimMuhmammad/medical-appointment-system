4bb1616251e88d14045c2102fb7069994799f051
Jimmy Truong <jimmy.truong@ucalgary.ca>
2018-06-13 18:49:42 -0600

    Convert Appointments to EJS

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,6 @@ module.exports = router;
 
 router.get('/', function(req, res) {
   res.render('pages/home');
-<<<<<<< Updated upstream
 });
 
 router.get('/login', function(req, res) {
@@ -27,34 +26,4 @@ router.get('/appointments', function(req, res) {
 
 router.post('appointment_submit.js' , function(req,res){
       //res.sendFile(path.join(__dirname + ''))
-=======
-});
-
-router.get('/login', function(req, res) {
-  res.render('pages/login');
-});
-
-router.get('/about', function(req, res){
-  res.render('pages/about');
-});
-
-router.get('/appointments', function(req,res){
-  res.render('pages/appointments');
-});
-
-router.get('/loggediin_header.html', function(req, res){
-  res.sendFile(path.join(__dirname + '/pages/navigation/loggediin_header.html'))
-});
-
-router.get('/home.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/css/home.css'))
-});
-
-router.get('/signin.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/signin.css'))
-});
-
-router.get('/footer.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/footer.css'))
->>>>>>> Stashed changes
 });
--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 // server.js
 
-<<<<<<< Updated upstream
-
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const app = express();
-const port = 3000;
-
-app.set('view engine', 'ejs');
-app.use(expressLayout);
-=======
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
@@ -19,16 +9,11 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
->>>>>>> Stashed changes
 
 // route our app
 const router = require('./app/routes.js');
 app.use('/', router);
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 // set static files (css and images, etc) location
 app.use(express.static(__dirname + '/public'));
 
