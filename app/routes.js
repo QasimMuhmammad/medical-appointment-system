e7f8d1957ca3e63133a43f7c73c822b3d53ba9b9
Jimmy Truong <jimmy.truong@ucalgary.ca>
2018-06-13 18:30:47 -0600

    Saving stash changes

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,16 @@
 // require express
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 // create our router object
-const router = express.Router()
-
-const path = require('path')
+const router = express.Router();
 
 // export our router
-module.exports = router
+module.exports = router;
 
 router.get('/', function(req, res) {
   res.render('pages/home');
+<<<<<<< Updated upstream
 });
 
 router.get('/login', function(req, res) {
@@ -27,4 +27,34 @@ router.get('/appointments', function(req, res) {
 
 router.post('appointment_submit.js' , function(req,res){
       //res.sendFile(path.join(__dirname + ''))
+=======
+});
+
+router.get('/login', function(req, res) {
+  res.render('pages/login');
+});
+
+router.get('/about', function(req, res){
+  res.render('pages/about');
+});
+
+router.get('/appointments', function(req,res){
+  res.render('pages/appointments');
+});
+
+router.get('/loggediin_header.html', function(req, res){
+  res.sendFile(path.join(__dirname + '/pages/navigation/loggediin_header.html'))
+});
+
+router.get('/home.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/css/home.css'))
+});
+
+router.get('/signin.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/navigation/signin.css'))
+});
+
+router.get('/footer.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/navigation/footer.css'))
+>>>>>>> Stashed changes
 });
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 
+<<<<<<< Updated upstream
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
@@ -8,15 +9,29 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(expressLayout);
+=======
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+>>>>>>> Stashed changes
 
 // route our app
 const router = require('./app/routes.js');
 app.use('/', router);
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 // set static files (css and images, etc) location
 app.use(express.static(__dirname + '/public'));
 
-
 app.listen(port, function(){
-	console.log("app started on port 3000");
-})
+	console.log("app started on port " + port);
+});
