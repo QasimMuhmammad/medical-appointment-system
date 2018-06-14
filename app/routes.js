d0ebc7f18837ee23995eeecd6c1807b580cb3a93
Jimmy Truong <jimmy.truong@ucalgary.ca>
2018-06-13 18:02:08 -0600

    Use EJS rendering

--- app/routes.js
+++ app/routes.js
@@ -4,61 +4,23 @@ const express = require('express')
 // create our router object
 const router = express.Router()
 
-
 const path = require('path')
-const router = express.Router()
 
 // export our router
 module.exports = router
 
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/home/home.html'))
+  res.render('pages/home');
 });
 
 router.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/login.html'))
-})
-
-router.get('/dashboard', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/dashboard.html'))
-})
-
-router.get('/header.html', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/header.html'))
-})
-
-router.get('/loggedin_header.html', function(req, res){
-  res.sendFile(path.join(__dirname + '/pages/navigation/loggedin_header.html'))
+  res.render('pages/login');
 });
 
-router.get('/footer.html', function(req, res) {
-  res.sendFile(path.join(__dirname + '/pages/navigation/footer.html'))
+router.get('/about', function(req, res) {
+  res.render('pages/about');
 });
 
-router.get('/appointments.html', function(req,res){
-  res.sendFile(path.joint(__dirname + '/pages/appointments.html'))
-});
-
-router.get('/home.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/css/home.css'))
-})
-
-router.get('/signin.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/css/signin.css'))
-})
-
-router.get('/footer.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/css/footer.css'))
-})
-
-router.get('/dashboard.css', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/css/dashboard.css'))
-})
-
-router.get('/appointments.html', function(req,res){
-  res.sendFile(path.join(__dirname + '/pages/appointments.html'))
-});
-
-router.get('/new_appointment.html' , function(req,res){
-  res.sendFile(path.join(__dirname + '/pages/new_appointment.html'))
+router.get('/appointments', function(req, res) {
+  res.render('pages/appointments');
 });
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-// index.js
+// server.js
 
-const express = require('express')
-const app = express()
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.use(expressLayout);
+
 // route our app
-const router = require('./routes.js')
-app.use('/', router)
+const router = require('./app/routes.js');
+app.use('/', router);
 
 // set static files (css and images, etc) location
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 
 app.listen(port, function(){
-	console.log("app started on port 3000")
+	console.log("app started on port 3000");
 })
