00484f002fb866c5d78e83d3db0ac1e581de71a2
Jimmy Truong <jimmy.truong@ucalgary.ca>
2018-06-22 07:54:30 -0600

    Save Commit

--- public/scripts/render-calendar-weekly.js
+++ public/scripts/render-calendar-weekly.js
@@ -2,12 +2,30 @@ $("#todays-date").append(function() {
   return "Today is " + moment().format("D MMMM, YYYY");
 });
 $(document).ready(function() {
+  console.log();
+  var bg = $(".calendar-tile");
+  var poppers = new Array();
+  for (var i = 0; i < 1; i++) {
+    //poppers.push(new Popper(bg[i], $(bg[i]).find('.popup'), {
+    //placement: 'top',
 
-      console.log("ready");
-  $(".btn-invis").click(function(event) {
-      console.log(event.target.id);
-    }
+    //}));
+    var a = $(bg[i]);
+    var b = $(bg[i]).find('.popup');
+    console.log(a);
+    console.log(b);
+
+  }
+
+  bg.click(function() {
+    var ref = $(this);
+    var popup = $(this).find('.popup');
+    var tooltip = new Tooltip(ref, {
+      placement: 'top'
+    });
 
 
-  );
+    console.log(tooltip);
+  });
+
 });
