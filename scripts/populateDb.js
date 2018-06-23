d5e1f1a01ab7d964b887c2e0c6b07d3f01ec8467
trvrle <32115579+trvrle@users.noreply.github.com>
2018-06-22 19:10:49 -0600

    Incomplete Table Data Insertions
    Co-Authored-By: Jimmy W. Truong <jimmy.truong@ucalgary.ca>

--- scripts/populateDb.js
+++ scripts/populateDb.js
@@ -6,6 +6,14 @@ const doctorsNote = require('../app/database/data/doctorsNote.json');
 const prescription = require('../app/database/data/prescription.json');
 const drug = require('../app/database/data/drug.json');
 
+const doctorTable = require('../app/database/schema/doctor.json');
+const receptionistTable = require('../app/database/schema/receptionist.json');
+const patientTable = require('../app/database/schema/patient.json');
+const appointmentTable = require('../app/database/schema/appointment.json');
+const doctorsNoteTable = require('../app/database/schema/doctorsNote.json');
+const prescriptionTable = require('../app/database/schema/prescription.json');
+const drugTable = require('../app/database/schema/drug.json');
+
 const mysql = require('mysql');
 const login = require('../credidentials.json');
 
@@ -19,21 +27,54 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   populateAll();
+  process.exit();
 });
 
 function populateAll() {
-  populate(doctor);
-  populate(receptionist);
-  populate(patient);
-  populate(appointment);
-  populate(doctorsNote);
-  populate(prescription);
-  populate(drug);
+  populate(doctor, doctorTable);
+  // populate(receptionist, receptionistTable);
+  // populate(patient);
+  // populate(appointment);
+  // populate(doctorsNote);
+  // populate(prescription);
+  // populate(drug);
 }
-//   var sql = "INSERT INTO patient (healthcarenum, fname,lname, sex, email) VALUES ('123456787', 'qasim2', 'muhammad','male', 'qasim.muhamad@ucalgary.ca')";
-populate(jsonData) {
-  var data = jsonData;
-  data.forEach(
-    var sql =
-  )
+var sql = "INSERT INTO patient (healthcarenum, fname, lname, sex, email) VALUES ('123456787', 'qasim2', 'muhammad','male', 'qasim.muhamad@ucalgary.ca')";
+
+function populate(jsonData, tableData) {
+  var data = jsonData.data;
+  var attr = tableData.attributes;
+  var tableName = tableData.name;
+
+  var colStrings = "";
+
+  var colStringArr = new Array();
+  for (var i = 0; i < attr.length - 1; i++) {
+    var column = attr[i].split(' ')[0];
+    colStrings += column + ", ";
+    colStringArr.push(column);
+  }
+  colStrings += attr[attr.length - 1].split(' ')[0];
+  colStringArr.push(attr[attr.length - 1].split(' ')[0]);
+
+  for (var i = 0; i < data.length; i++) {
+    var colValues = "";
+    for (var j = 0; j < attr.length - 1; j++) {
+      colValues += "'" + data[i][colStringArr[j]] + "', ";
+    }
+    colValues += "'" + data[i][colStringArr[attr.length - 1]] + "')";
+
+
+    var sql = "INSERT INTO " + tableName + " (";
+    sql += colStrings + ") VALUES (" + colValues;
+
+    con.query(sql, function(err, result) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+    });
+  }
+  console.log("Data for table " + tableName + " added. " + data.length +
+    " records inserted.");
 }
