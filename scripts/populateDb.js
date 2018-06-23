a265d36e12b871eeb5ed9a8a9e135c5c094bf4b1
trvrle <32115579+trvrle@users.noreply.github.com>
2018-06-22 18:36:27 -0600

    Saving Data
    Co-Authored-By: Jimmy W. Truong <jimmy.truong@ucalgary.ca>

--- scripts/populateDb.js
+++ scripts/populateDb.js
@@ -3,7 +3,6 @@ const receptionist = require('../app/database/data/receptionist.json');
 const patient = require('../app/database/data/patient.json');
 const appointment = require('../app/database/data/appointment.json');
 const doctorsNote = require('../app/database/data/doctorsNote.json');
-const doctorsPhoneNumber = require('../app/database/data/doctorsPhoneNumber.json');
 const prescription = require('../app/database/data/prescription.json');
 const drug = require('../app/database/data/drug.json');
 
@@ -19,5 +18,22 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-
+  populateAll();
 });
+
+function populateAll() {
+  populate(doctor);
+  populate(receptionist);
+  populate(patient);
+  populate(appointment);
+  populate(doctorsNote);
+  populate(prescription);
+  populate(drug);
+}
+//   var sql = "INSERT INTO patient (healthcarenum, fname,lname, sex, email) VALUES ('123456787', 'qasim2', 'muhammad','male', 'qasim.muhamad@ucalgary.ca')";
+populate(jsonData) {
+  var data = jsonData;
+  data.forEach(
+    var sql =
+  )
+}
